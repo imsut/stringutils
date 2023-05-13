071938295bad111a9ec24216cf75674f804b9581
Ken Kawamoto <kentaro.kawamoto@gmail.com>
2023-05-13 13:32:58 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/imsut/stringutils
+
+go 1.19
---
+++ libs.go
@@ -0,0 +1,13 @@
+package libs
+
+import (
+	"strings"
+)
+
+func Upper(s string) string {
+	return strings.ToUpper(s)
+}
+
+func Lowerr(s string) string {
+	return strings.ToLower(s)
+}
